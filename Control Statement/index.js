2fff3220f358ebe715a5a421d97df7fbae9a760b
Pranit <patilpranit3112@gmail.com>
2023-11-22 21:30:34 +0530

    Basic Questions

---
+++ Control Statement/index.js
@@ -0,0 +1,50 @@
+/* 
+
+// Question-1 :   WAP that checks if a variable x is greater than 10. If it is, log "x is greater than 10," otherwise, log "x is not greater than 10."
+
+let x = 15;
+
+if (x > 10) {
+  console.log("The value of x is greater than 10");
+} else {
+  console.log("The value of x is not greater than 10");
+}
+
+x > 10
+  ? console.log("The value of x is greater than 10")
+  : console.log("The value of x is not greater than 10");
+
+// Question-2 :   WAP that check that if an user is illegible for drive a car or not ( take user age as input).
+
+let age = prompt("Please enter your age");
+
+if (age > 18) {
+  console.log("You are legible for drive a car");
+} else {
+  console.log("You are illegible for drive a car");
+}
+
+// Question-3:    WAP to assign grade for a student For example, if the score is between 90 and 100, assign the grade "A.", (take score of every subject as input).
+
+let score = prompt("Please Enter the Score");
+
+if (score <= 90 || score >= 100) {
+  console.log("the grade A");
+}
+
+// Question-4:   WAP to check  time of day based on the current hour. For example, if it's before noon, log "Good morning.". (take time as an input).
+
+let date = new Date().getHours;
+
+console.log(date);
+
+if (date < 12) {
+  console.log("Good Morning");
+} else {
+  console.log("Good Night");
+}
+
+
+*/
+
+
